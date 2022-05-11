664ec1c5b9b3288358e5554f880bd0cd39d39a70
Deniss Dmitrijevs <dod3005@gmail.com>
2022-05-11 10:27:10 +0300

    Add project files.

---
+++ BezierLine/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace BezierLine
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pointCountTextBox = new System.Windows.Forms.TextBox();
+            this.pointCountButton = new System.Windows.Forms.Button();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pointCountTextBox
+            // 
+            this.pointCountTextBox.Location = new System.Drawing.Point(629, 12);
+            this.pointCountTextBox.Name = "pointCountTextBox";
+            this.pointCountTextBox.Size = new System.Drawing.Size(159, 23);
+            this.pointCountTextBox.TabIndex = 0;
+            // 
+            // pointCountButton
+            // 
+            this.pointCountButton.Location = new System.Drawing.Point(629, 41);
+            this.pointCountButton.Name = "pointCountButton";
+            this.pointCountButton.Size = new System.Drawing.Size(159, 23);
+            this.pointCountButton.TabIndex = 1;
+            this.pointCountButton.Text = "Enter new point count";
+            this.pointCountButton.UseVisualStyleBackColor = true;
+            this.pointCountButton.Click += new System.EventHandler(this.pointCountButton_Click);
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(629, 70);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(159, 23);
+            this.drawButton.TabIndex = 2;
+            this.drawButton.Text = "Draw Bezier curve";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.pointCountButton);
+            this.Controls.Add(this.pointCountTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pointCountTextBox;
+        private System.Windows.Forms.Button pointCountButton;
+        private System.Windows.Forms.Button drawButton;
+    }
+}
+
---
+++ BezierLine/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BezierLine
+{
+    public partial class Form1 : Form
+    {
+        private const double Step = 0.01;
+        private int pointCount = 3;
+        private List<Point> points = new List<Point>();
+        private Graphics graphics;
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = this.CreateGraphics();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (points.Count < pointCount)
+            {
+                int x = this.PointToClient(Cursor.Position).X;
+                int y = this.PointToClient(Cursor.Position).Y;
+                graphics.DrawRectangle(new Pen(Color.Black, 4), new Rectangle(x, y, 1, 1));
+
+                points.Add(new Point(x, y));
+            } 
+            else
+            {
+                MessageBox.Show("Point count exceeded, enter bigger point count");
+            }
+        }
+
+        private void pointCountButton_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(pointCountTextBox.Text, out pointCount))
+            {
+                if (pointCount < 2)
+                {
+                    MessageBox.Show("Point count must be bigger than or equal to two");
+                }
+                else
+                {
+                    graphics.Clear(Color.LightGray);
+                    points = new List<Point>();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Point count must be an integer number");
+            }
+        }
+
+        private void DrawBezierCurve(List<Point> controlPoints)
+        {
+            Point prevPoint = controlPoints.ElementAt(0);
+            int n = controlPoints.Count;
+            for (double t = Step; t <= 1; t += Step)
+            {
+                double x = 0, y = 0;
+                double Bernstein;
+                for (int i = 0; i < n; ++i)
+                {
+                    Bernstein = CalculateBernsteinPolynomValue(n - 1, i, t);
+                    x += Bernstein * controlPoints[i].X;
+                    y += Bernstein * controlPoints[i].Y;
+
+                }
+
+                Point newPoint = new Point((int) x, (int) y);
+
+                graphics.DrawLine(new Pen(new SolidBrush(Color.Red)), prevPoint, newPoint);
+                prevPoint = newPoint;
+            }
+            graphics.DrawLine(new Pen(new SolidBrush(Color.Red)), prevPoint, controlPoints.ElementAt(controlPoints.Count - 1));
+        }
+
+        private double CalculateBernsteinPolynomValue(int n, int i, double t)
+        {
+            double value = (Factorial(n) * Math.Pow(1.0 - t, n - i) * Math.Pow(t, i)) / (Factorial(i) * Factorial(n - i));
+            return value;
+        }
+
+        private long Factorial(int n)
+        {
+            long factorial = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            if (points.Count > 1)
+            {
+                DrawBezierCurve(points);
+            } 
+        }
+    }
+}
